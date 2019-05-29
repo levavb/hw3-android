05ecdf7e3fefa1c7374c86f3192e5e7514f35dbe
AfekSurdo <48516118+AfekSurdo@users.noreply.github.com>
2019-05-29 14:47:04 +0300

    Update complimentsPage.java
    ComplimentsPage

--- app/src/main/java/com/example/triviaapp/complimentsPage.java
+++ app/src/main/java/com/example/triviaapp/complimentsPage.java
@@ -1,26 +1,27 @@
-package com.example.triviaapp;
+package com.example.triviaapp; 
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.content.Intent; 
+import android.support.v7.app.AppCompatActivity; 
+import android.os.Bundle; 
+import android.view.View; 
+import android.widget.TextView; 
 
-public class complimentsPage extends AppCompatActivity {
+public class complimentsPage extends AppCompatActivity { 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compliments_page);
 
         Intent intent = getIntent();
         String userN = intent.getStringExtra("userName");
         TextView tv1 = findViewById(R.id.tv1);
-        tv1.setText("Congratulations "+ userN);
-    }
-
-    public void goToHomePage(View view) {
-        Intent intent = new Intent(complimentsPage.this,MainActivity.class);
-        startActivity(intent);
-    }
-}
+        tv1.setText("Congratulations "+ userN); 
+    } 
+ 
+    public void goToHomePage(View view) { 
+        Intent intent = new Intent(complimentsPage.this,MainActivity.class); 
+        startActivity(intent); 
+    } 
+} 
